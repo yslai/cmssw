0a239690ca4317e4170ac20ed39f36cb4ef99ac9
Yetkin <yetkin.yilmaz@cern.ch>
2015-01-23 16:57:50 +0100

    filling other members

--- RecoHI/HiJetAlgos/plugins/UETableProducer.cc
+++ RecoHI/HiJetAlgos/plugins/UETableProducer.cc
@@ -133,9 +133,9 @@ UETableProducer::endJob() {
   unsigned int Nni2 = ni2[0]*ni2[1];
 
   std::copy(np, np + 5, ue_predictor_pf->np.begin());
-  std::copy(Nni0, Nni0 + 2, ue_predictor_pf->ni0.begin());
-  std::copy(Nni1, Nni1 + 2, ue_predictor_pf->ni1.begin());
-  std::copy(Nni2, Nni2 + 2, ue_predictor_pf->ni2.begin());
+  std::copy(ni0, ni0 + 2, ue_predictor_pf->ni0.begin());
+  std::copy(ni1, ni1 + 2, ue_predictor_pf->ni1.begin());
+  std::copy(ni2, ni2 + 2, ue_predictor_pf->ni2.begin());
 
   static const float edge_pseudorapidity[16] = {
 	-5.191, -2.650, -2.043, -1.740, -1.479, -1.131, -0.783, -0.522, 0.522, 0.783, 1.131, 1.479, 1.740, 2.043, 2.650, 5.191
